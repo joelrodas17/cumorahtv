b0f9d32001170d9f04da3847ac5f6c107be40a58
Joel Rodas Sánchez <joel.rodas.sanchez@gmail.com>
2023-05-19 08:32:59 -0500

    asdasd

--- video.js
+++ video.js
@@ -13,35 +13,34 @@ function iraVideo(indice){
   document.getElementById("iframe").height = altoIframe;
   switch(indice){
     case 1: 
-            document.getElementById("fondo").style.backgroundImage = 'url("img/otroladodelcielo2.jpg")';
             break;
     case 2:
-            document.getElementById("fondo").style.backgroundImage = 'url("img/otroladodelcielo1.png")';
+        
             document.getElementById("iframe").src = "https://www.youtube.com/embed/i9RuCJTevnc?start=5";
             break;
     case 3: 
-            document.getElementById("fondo").style.backgroundImage = 'url("img/mostervsmormones.png")';
+           
             document.getElementById("iframe").src = "https://www.youtube.com/embed/A153VZ4Zimg?start=48";
             break;
             break;
     case 4:
-            document.getElementById("fondo").style.backgroundImage = 'url("img/charlie.png")';
+           
             document.getElementById("iframe").src = "https://www.youtube.com/embed/2l6m5NQHM3A?start=4";
             break;
     case 5: 
-            document.getElementById("fondo").style.backgroundImage = 'url("img/therm.png")';
+            
             document.getElementById("iframe").src = "https://www.youtube.com/embed/93htaSyBlwg";
             break;
     case 6:
-            document.getElementById("fondo").style.backgroundImage = 'url("img/recadodeangeles.png")';
+           
             document.getElementById("iframe").src = "https://www.youtube.com/embed/r4ljBABcEZk?start=5";
             break;
     case 7:
-            document.getElementById("fondo").style.backgroundImage = 'url("img/kenedihadson.png")';
+           
             document.getElementById("iframe").src = "https://www.youtube.com/embed/YC105Z4MCrs?start=2";
             break;
     case 8:
-            document.getElementById("fondo").style.backgroundImage = 'url("img/tesorodehombre.png")';
+            
             document.getElementById("iframe").src = "https://www.youtube.com/embed/dHdeMsutk1Y?start=5";
             break;
     default:
@@ -49,7 +48,7 @@ function iraVideo(indice){
   }
 }
 reproducir.addEventListener('click', function() {
-
+        
         if (iframe.requestFullscreen) {
           iframe.requestFullscreen();
         } else if (iframe.mozRequestFullScreen) { // Para Firefox
